c4232e1a792391aa17336270fbb7e2a21d6f5e2f
literaiiy <liddery69@gmail.com>
2024-07-28 01:58:49 -0700

    Metadata/OG update

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -1,21 +1,34 @@
 import { getPageBySlug, getPages } from "@/lib/api"
+import Head from "next/head";
+import { Metadata } from "next";
 
 export default async function Page({ params }: { params: { page: string } }) {
   const page = await getPageBySlug(params.page);
-
+  
   return (
-    <main>
-      <div>
-        {params.page}
-        {page.title.rendered}
-      </div>
-      {/* <div>{page.content}</div> */}
-      <div dangerouslySetInnerHTML={{ __html: page.content.rendered}} />
-    </main>
+    <>
+      <Head>
+        <title>{page[0].title.rendered} | ITE at UC Irvine</title>
+      </Head>
+      <main>
+        <div>
+          <h1>{page[0].title.rendered}</h1>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: page[0].content.rendered}} />
+      </main>
+    </>
   )
 }
 
-export async function generateStaticParams() {
-  const pages = await getPages();
-  return pages.map((page: any) => ({ params: { page: page.slug } }));
+export async function generateMetadata({ params }: {params: {page: string}}): Promise<Metadata> {
+  const page = await getPageBySlug(params.page);
+
+  return {
+    title: page[0].title.rendered,
+  }
 }
+
+// export async function generateStaticParams() {
+//   const pages = await getPages();
+//   return pages.map((page: any) => ({ params: { page: page.slug } }));
+// }
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,23 @@
 const API_URL = "https://sites.uci.edu/irvineite/wp-json/wp/v2";
 
-import axios from "axios";
+// import axios from "axios";
 
-export const getPages = async () => {
-  const res = await axios.get(`${API_URL}/pages`);
-  return res.data;
+// export const getPages = async () => {
+//   const res = await axios.get(`${API_URL}/pages`);
+//   return res.data;
+// }
+
+// export const getPageBySlug = async (slug: string) => {
+//   const res = await axios.get(`${API_URL}/pages`, { params: { slug } });
+//   return res.data[0];
+// }
+
+export const getPages = async() => {
+  const res = await fetch(`${API_URL}/pages`);
+  return res.json();
 }
 
-export const getPageBySlug = async (slug: string) => {
-  const res = await axios.get(`${API_URL}/pages`, { params: { slug } });
-  return res.data[0];
+export const getPageBySlug = async(slug: string) => {
+  const res = await fetch(`${API_URL}/pages?slug=${slug}`);
+  return res.json();
 }
