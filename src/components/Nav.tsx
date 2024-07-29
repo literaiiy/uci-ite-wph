2cee65e9865818761504b9a85ae5bb027360baa3
literaiiy <liddery69@gmail.com>
2024-07-28 20:47:58 -0700

    Navbar update 2

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,11 +9,13 @@ export default async function Nav() {
 
   return (
     <nav>
-      <a href="/" className="logo">
-        <span className="uci">uci</span>
-        <Image alt="UCI ITE logo" src="/anteater.svg" width={ANTEATER_SIZE} height={ANTEATER_SIZE * 0.66}/>
-        <span className="ite">ite</span>
-      </a>
+      <div className="logo-wrapper">
+        <a href="/" className="logo">
+          <span className="uci">uci</span>
+          <Image alt="UCI ITE logo" src="/anteater.svg" width={ANTEATER_SIZE} height={ANTEATER_SIZE * 0.66}/>
+          <span className="ite">ite</span>
+        </a>
+      </div>
       {/* <div className="sub">Institute of Transportation Engineers at the University of California, Irvine</div> */}
       <div className="nav-items">
         {pages.map((page: any) => (
