0b2f37192555b5e454494c76534091758d22b551
literaiiy <liddery69@gmail.com>
2024-08-07 19:55:40 -0700

    Navbar ordering change - now sorts by WP `Order`

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,18 +5,28 @@ export const headers = { headers: {
   "Cache-Control": "no-cache"
 }};
 
-
+/**
+ * Gets a list of pages, ordered by the "Order" property set in Wordpress.
+ */
 export const getPages = async(): Promise<object[]> => {
-  const res = await fetch(`${API_URL}/pages`, headers);
+  const res = await fetch(`${API_URL}/pages?orderby=menu_order&order=asc`, headers);
   return await removeFrontpage(await res.json());
 }
 
+/**
+  * Gets page by slug.
+  * 
+  * @param slug - The slug of the page to get.
+  * @returns The page with the specified slug.
+  */
 export const getPageBySlug = async(slug: string): Promise<any> => {
   const res = await fetch(`${API_URL}/pages?slug=${slug}`, headers);
   return res.json();
 }
 
-// Gets media from gallery with description that includes specified string.
+/**
+ * Gets media from gallery with description that includes specified string.
+ */
 export const getSpecificMedia = async(description: string): Promise<[string, string, string][]> => {
   const res = await fetch(`${API_URL}/media?search=${description}`, headers);
   const json = await res.json();
@@ -28,6 +38,10 @@ export const getSpecificMedia = async(description: string): Promise<[string, str
   return parsed;
 }
 
+/**
+ * Removes the frontpage from the list of pages.
+ * *Necessary for the current implementation of the carousel
+ */
 const removeFrontpage = (pages: any) => {
   return pages.filter((page: any) => page.slug !== "frontpage");
 }
