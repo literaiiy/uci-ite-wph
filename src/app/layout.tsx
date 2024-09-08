5dfdf70d801e82b4f51672878079c7bca27e44a0
literaiiy <liddery69@gmail.com>
2024-09-08 02:15:33 -0700

    hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={defaultFont.className}>
         <Banner/>
+        {/* @ts-ignore */}
         <Nav pages={pages} />
         {children}
         <Footer />
