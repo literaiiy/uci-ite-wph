81e9cedfa3d855786b5b5e223135337e64644e9a
literaiiy <liddery69@gmail.com>
2025-05-03 15:42:26 -0700

    1st draft banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={defaultFont.className}>
-        {/* <Banner/> */}
+        <Banner/>
         {/* @ts-ignore */}
         <Nav pages={pages} />
         {children}
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,8 @@ import { RiToolsLine } from 'react-icons/ri';
 export default function Banner() {
   return (
     <div className="banner">
-      <span>This site is currently under construction. View the stable site <a href="https://sites.uci.edu/irvineite/">here</a>.</span>
+      {/* <span>This site is currently under construction. View the stable site <a href="https://sites.uci.edu/irvineite/">here</a>.</span> */}
+      <span>ITE at UCI is hosting the Western District Student Leadership Summit from January 23rd to 25th, 2026. Stay tuned! 🍊</span>
     </div>
   )
 }
